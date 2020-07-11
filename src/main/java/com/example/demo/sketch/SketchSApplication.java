74a975c286b5f9143ac43bc02a3a292ae5c25777
Min Wang <ustcace@mail.ustc.edu.cn>
2020-07-11 16:16:59 +0800

    feat: 添加Spring Boot Sketch项目配置文件

---
+++ src/main/java/com/example/demo/sketch/SketchSApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.sketch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SketchSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SketchSApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo/sketch/SketchSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.sketch;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SketchSApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
